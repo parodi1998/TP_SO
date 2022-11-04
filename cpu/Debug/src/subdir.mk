3b9b5221857ed0a638788c2ac0bdfa66b0b8a5e3
MatiasRosales <matias.rosales97@hotmail.com>
2022-11-04 20:29:12 -0300

    Debug Files

--- cpu/Debug/src/subdir.mk
+++ cpu/Debug/src/subdir.mk
@@ -5,42 +5,44 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../src/client.c \
+../src/client_memoria.c \
 ../src/conexiones.c \
+../src/config_cpu.c \
 ../src/instrucciones.c \
 ../src/main.c \
 ../src/server.c \
+../src/tlb.c \
 ../src/utils.c 
-
-C_DEPS += \
-./src/client.d \
-./src/conexiones.d \
-./src/instrucciones.d \
-./src/main.d \
-./src/server.d \
-./src/utils.d 
 
 OBJS += \
 ./src/client.o \
+./src/client_memoria.o \
 ./src/conexiones.o \
+./src/config_cpu.o \
 ./src/instrucciones.o \
 ./src/main.o \
 ./src/server.o \
+./src/tlb.o \
 ./src/utils.o 
+
+C_DEPS += \
+./src/client.d \
+./src/client_memoria.d \
+./src/conexiones.d \
+./src/config_cpu.d \
+./src/instrucciones.d \
+./src/main.d \
+./src/server.d \
+./src/tlb.d \
+./src/utils.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.c src/subdir.mk
+src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	gcc -I/home/utnso/git/tp-2022-2c-Los_OlvidadOS/cpu/src -ICommons -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	gcc -I/home/utnso/git/tp-2022-2c-Los_OlvidadOS/cpu/src -ICommons -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-
-clean: clean-src
-
-clean-src:
-	-$(RM) ./src/client.d ./src/client.o ./src/conexiones.d ./src/conexiones.o ./src/instrucciones.d ./src/instrucciones.o ./src/main.d ./src/main.o ./src/server.d ./src/server.o ./src/utils.d ./src/utils.o
-
-.PHONY: clean-src
 
